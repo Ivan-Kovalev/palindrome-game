84b18249842e515203026649e79a600c2720a0ec
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-12-19 21:32:40 +0300

    master

---
+++ src/main/java/com/example/palindrome_game/PalindromeGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.palindrome_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalindromeGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalindromeGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/palindrome_game/PalindromeGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.palindrome_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalindromeGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
